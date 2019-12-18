4d0623062445c48d44e8f6aaa0c686b1f7f5bf9b
张富国、 <fuguo.zhang@jyblife.com>
2019-12-18 19:28:42 +0800

    feat 实现认证服务器存储

--- oauth-center/auth-server/src/main/java/com/zss/microservices/AuthServerApp.java
+++ oauth-center/auth-server/src/main/java/com/zss/microservices/AuthServerApp.java
@@ -1,4 +1,12 @@
 package com.zss.microservices;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class AuthServerApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServerApp.class, args);
+    }
 }
